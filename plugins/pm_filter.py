e47c115b94c734eedf75c11c1575d49c8bec5eb8
Creatornk <116665898+Creatornk@users.noreply.github.com>
2022-10-26 18:20:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -858,7 +858,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"🖥TITLE :- {search} \n 🌬user :- {user} \n ♻️join :- @NKBACKUPCHANNEL"
+        cap = f"🖥TITLE :- {search} \n ♻️join :- @NKBACKUPCHANNEL"
     if imdb and imdb.get('poster'):
         try:
           a = await message.reply_photo(photo="https://te.legra.ph/file/4bde81bfc6ab8bc34cdba.jpg", caption=cap[:1024],
