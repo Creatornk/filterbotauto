b7467eb41c7e3665a3a4663c297a4148d3c104fe
Creatornk <116665898+Creatornk@users.noreply.github.com>
2022-11-01 20:17:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -986,3 +986,7 @@ async def manual_filters(client, message, text=False):
                 break
     else:
         return False
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 10))
+SELF_DELETE = environ.get('SELF_DELETE', True)
+if SELF_DELETE == "True":
+    SELF_DELETE = True
